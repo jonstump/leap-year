7e2cb644dab8511ae98142d79b5f43d5d00b66ec
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-20 10:43:31 -0800

    fix bug with undefined variable due to extra bracket
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 
 function isLeapYear(year) {
-  } if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
+}
