048eac5312c1ea87df5803c33e2a774844d2ef44
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-20 11:44:28 -0800

    page will say true or false if the entered year is a leap year
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
+$(document).ready(function() {
+  $("#leapYear").submit(function(event) {
+    $("#result").hide();
+    event.preventDefault();
+    const year = parseInt($("input#year").val());
+    const result = isLeapYear(year)
+    console.log(result)
 
-function isLeapYear(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
-}
+    function isLeapYear(year) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    $('span.result').text(result);
+    $("#result").show();
+
+  });
+});
